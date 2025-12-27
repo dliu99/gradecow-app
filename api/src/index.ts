f3edb33c102f8275e66e468a28e99a6f2cebc9ba
dliu99 <devin78988@gmail.com>
2025-12-27 13:00:56 -0800

    schools endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,7 +3,18 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({ ok: true,message: 'Hello Hono!' })
+})
+
+app.get('/ic/districts', async (c) => {
+  const state = c.req.query('state');
+  const query = c.req.query('query');
+  if (!state || !query) {
+    return c.json({ ok: false, message: 'State and query are required' }, 400)
+  }
+  const response = await fetch(`https://www.infinitecampus.com/api/district?state=${state.toUpperCase()}&query=${query}`)
+  const data = await response.json()
+  return c.json({ ok: true, data })
 })
 
 export default app
